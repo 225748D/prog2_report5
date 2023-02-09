fe533976abcde78faa3f7b6531aa1d89357365d9
Kento nakasone <e225748@ie.u-ryukyu.ac.jp>
2023-02-09 23:19:03 +0900

    STEP2

--- Report5.java
+++ Report5.java
@@ -1,7 +1,13 @@
+import java.lang.NullPointerException;
+
 public class Report5{
 
 public static void main(String[] args) {
-String str = null;
-str.length();
+    try{
+        String str = null;
+        System.out.println(str.length());
+    }catch(NullPointerException e){
+        System.out.println("NullPointerExceptionが発生しています\n" + e.getMessage());
+    }
     }
 }
